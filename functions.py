9c8204b65f1379c5d567d9c9f3f23a58f1192271
PolinaWednesday <kekssmail@gmail.com>
2023-08-07 13:25:37 +0300

    hello python

---
+++ functions.py
@@ -0,0 +1,47 @@
+import random
+
+
+def len_words_from_file(path):
+    """
+    Функция читающая список слов.
+    :param path: Путь до файла.
+    :return: Возвращает слово.
+    """
+    with open(path, 'r', encoding='utf8') as file:
+       words = file.read().strip().split("\n")
+    return words
+
+
+def shuffle_word(word):
+    """"""
+    word_list = list(word)
+    random.shuffle(word_list)
+    return ''.join(word_list)
+
+
+def record_player(path, player_name, player_count):
+    """
+    Функция записи игрока в файл.
+    :param path: Путь до файла.
+    :param player_name: Имя игрока.
+    :param player_count: Очки игрока.
+    :return: None.
+    """
+    with open(path, 'a', encoding='utf8') as file:
+        file.write(f'{player_name}  {player_count} \n')
+
+
+def read_history(path):
+    max = 0
+    count = 0
+
+    with open(path, "r", encoding="utf8") as file:
+        for line in file.readlines():
+            count += 1
+            score = int(line.split("  ")[1])
+
+            if score > max:
+                max = score
+
+    return f"Всего игр сыграно: {count}\n" \
+           f"Максимальный рекорд: {max}"
--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello")
+from functions import len_words_from_file, shuffle_word, record_player, read_history
+
+
+words_txt = 'words.txt'
+history_txt = 'history.txt'
+words = len_words_from_file('words.txt')
+user_name = input("Введите ваше имя: ")
+count = 0
+for word in words:
+    shuffle_word(word)
+    user_answer = input(f'Угадайте слово: {shuffle_word(word)} ')
+    if user_answer.lower().strip(" ") == word:
+        print(f"Верный ответ! Вы получаете: 10 балов")
+        count += 10
+    else:
+        print(f"Неверно! Верный ответ – {word}")
+
+record_player(history_txt, user_name, count)
+print(read_history(history_txt))
